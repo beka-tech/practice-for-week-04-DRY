41172cf2e2426fc1f541a5d98275d70e1fa9d74d
beka-tech <abeka039@gmail.com>
2025-11-12 15:31:47 +0300

    DRY
    DRY programming Principles

--- practice.js
+++ practice.js
@@ -1,47 +1,20 @@
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  return num1 > num2 ? num1 : num2;
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  return num1 > num2 ? num1 / 3 : num2 / 3;
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  return sum1 > sum2 ? `I ate${sum1}` : `I ate${sum2}`;
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
+  return weight1 < weight2
+    ? `I adopted a dog that weighs ${weight1} pounds.`
+    : `I adopted a dog that weighs ${weight2} pounds.`;
 }
-
 
 /**************************************************************************/
 /* DO NOT CHANGE THE CODE BELOW */
@@ -49,5 +22,5 @@ module.exports = {
   multiplyBiggerNumByTwo,
   divideBiggerNumByThree,
   eatMostTacos,
-  adoptSmallerDog
+  adoptSmallerDog,
 };
